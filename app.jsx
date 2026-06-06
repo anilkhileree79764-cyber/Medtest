5264d7a8986bb1da8f191e7aecc4b9c2e8b30d01
anilkhileree79764-cyber <anilkhileree79764@gmail.com>
2026-06-06 11:02:20 +0530

    Create app.jsx

---
+++ app.jsx
@@ -0,0 +1,4 @@
+// MedTest App - Loading
+export default function App() {
+  return <div style={{color:"white",background:"#0a0f1e",height:"100vh",display:"flex",alignItems:"center",justifyContent:"center",fontSize:24}}>🩺 MedTest Loading...</div>
+}
